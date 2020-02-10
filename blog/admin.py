3cb4b2f7ab9f291336ca89eac5dff7fa2483296a
Peter Munyua <munyuapeter07@gmail.com>
2020-02-10 08:19:22 +0300

    added comment system the blog apllication

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
-
+from .models import Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -13,3 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,10 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from django.core.paginator import Paginator, EmptyPage, \
-    PageNotAnInteger
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
-# function based view
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts in each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'page': page,
-#                    'posts': posts})
-
-# class based view
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -39,9 +18,27 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # list all active comments for a single post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # assign the current posst to the comment
+            new_comment.post = post
+            # save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 def post_share(request, post_id):
@@ -53,7 +50,6 @@ def post_share(request, post_id):
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            print('hello')
             # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
@@ -61,8 +57,8 @@ def post_share(request, post_id):
             message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
             send_mail(subject, message, 'munyuapeter07@gmail.com', [cd['to']])
             sent = True
-            return render(request, 'blog/post/share.html', {'sent': sent})
     else:
         form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
