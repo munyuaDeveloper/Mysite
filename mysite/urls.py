6e20b965ad1ecb85e36c4b5a8dcb7a239b138f1b
Peter Munyua <munyuapeter07@gmail.com>
2020-02-20 11:11:36 +0300

    added template tags in my blog application

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    # path('admin/', admin.site.urls),
+    # path('blog/', include('blog.urls', namespace='blog')),
+    url(r'^admin/', admin.site.urls),
+    url(r'blog/', include('blog.urls', namespace='blog')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
